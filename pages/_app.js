fdc4f30d7437d8cfcfca446f4b72be04069121bb
uguremirmustafa <uguremirmustafa@gmail.com>
2021-03-01 17:40:04 +0300

    initial

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,21 @@
-import '../styles/globals.css'
+import { Provider } from 'next-auth/client';
+import '../styles/globals.css';
+import { DataProvider } from '@context/GlobalState';
+import Layout from '@components/layout/Layout';
+import { initRouterListeners } from '@utils/scrollRestoration';
+
+initRouterListeners();
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <DataProvider>
+      <Provider session={pageProps.session}>
+        <Layout>
+          <Component {...pageProps} />
+        </Layout>
+      </Provider>
+    </DataProvider>
+  );
 }
 
-export default MyApp
+export default MyApp;
